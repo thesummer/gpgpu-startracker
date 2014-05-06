85c2a34e92a326bdb93ff8e24b6d641c5e281c8a
Jan Sommer <summerspam@web.de>
2014-05-07 00:21:31 +0300

    Print x,y image coordinates into rgba buffer with 16bit precision
    Add 2 helper functions to shader:
    
    pack2shorts: takes to floating point numbers (integer values assumed)
    and packs them into a vec4 as 16bit unsigned integers
    
    tex2imgCoord: computes the image coordinates ([0..texWidth],
    [0..texHeight] from the texel coordinates

--- fragShader.glsl
+++ fragShader.glsl
@@ -3,8 +3,26 @@
 precision highp float;
 varying vec2 v_texCoord;
 uniform sampler2D s_texture;
+uniform vec2 u_texDimensions;
+
+/*
+
+
+*/
+vec4 pack2shorts(in vec2 shorts)
+{
+    shorts /= 256.0f;
+    return vec4(floor(shorts)/255.0f, fract(shorts)*256.0f/255.0f).zxwy;
+}
+
+vec2 tex2imgCoord(in vec2 texCoord)
+{
+    return (2.0f*texCoord*u_texDimensions-1.0f)/2.0f;
+}
 
 void main()
 {
-  gl_FragColor = texture2D( s_texture, v_texCoord );
+    vec2 imgCoord = tex2imgCoord(v_texCoord);
+    gl_FragColor = pack2shorts( imgCoord);
+//    gl_FragColor = texture2D( s_texture, v_texCoord );
 }
