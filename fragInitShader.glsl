2cbbf00bf3f0555434a66678531a482f6ea6934d
Jan Sommer <summerspam@web.de>
2014-05-15 17:31:40 +0300

    Shader for threshold and initial label operation

--- fragInitShader.glsl
+++ fragInitShader.glsl
@@ -1,9 +1,10 @@
 
 
 precision highp float;
-varying vec2 v_texCoord;
-uniform sampler2D s_texture;
-uniform vec2 u_texDimensions;
+varying vec2 v_texCoord;        // texture coordinates
+uniform sampler2D s_texture;    // texture sampler
+uniform vec2 u_texDimensions;   // image/texture dimensions
+uniform float u_threshold;      // threshold value for the threshold operation
 
 /*
 Assuming that the texture is 8-bit RGBA 32bits are available for packing.
@@ -48,9 +49,26 @@ vec2 tex2imgCoord(in vec2 texCoord)
     return (2.0f*texCoord*u_texDimensions-1.0f)/2.0f;
 }
 
+
+/*
+Shader will do the following steps for each pixel:
+    1. Threshold with u_threshold
+    2. pack image coordinates for into RGBA value for non-zero pixels
+
+    It is assumed that the input texture is a gray scale RGBA-image (c,c,c,255)
+*/
 void main()
 {
+    // Compute image coordinates
     vec2 imgCoord = tex2imgCoord(v_texCoord);
-    gl_FragColor = pack2shorts( imgCoord);
-//    gl_FragColor = texture2D( s_texture, v_texCoord );
+
+    // Get the input pixel value
+    gl_FragColor = texture2D( s_texture, v_texCoord );
+
+    // Threshold operation
+    gl_FragColor.a = gl_FragColor.r;
+    gl_FragColor = step(vec4(u_threshold), gl_FragColor);
+
+    // pack image coordinates for the non-zero pixels
+    gl_FragColor = pack2shorts(imgCoord) * gl_FragColor;
 }
