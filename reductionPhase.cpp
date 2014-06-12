7532cf3c13d030286368047b3b7b26c1d0c31404
Jan Sommer <summerspam@web.de>
2014-06-12 15:21:56 +0300

    Add class-files for the reduction phase

---
+++ reductionPhase.cpp
@@ -0,0 +1,101 @@
+#include "reductionPhase.h"
+
+ReductionPhase::ReductionPhase(int width, int height)
+    :mVertFilename("vertShader.glsl"), mFragFilename("fragReductionShater.glsl"),
+      mWidth(width), mHeight(height),
+      mVertices {-1.0f, -1.0f, 0.0f,  // Position 0
+                  0.0f,  0.0f,        // TexCoord 0
+                 -1.0f,  1.0f, 0.0f,  // Position 1
+                  0.0f,  1.0f,        // TexCoord 1
+                  1.0f,  1.0f, 0.0f,  // Position 2
+                  1.0f,  1.0f,        // TexCoord 2
+                  1.0f, -1.0f, 0.0f,  // Position 3
+                  1.0f,  0.0f         // TexCoord 3
+                },
+      mIndices { 0, 1, 2, 0, 2, 3 }
+
+{
+}
+
+GLint ReductionPhase::init(GLuint fbos[], GLuint initTexture,
+                           int numNewTextures, GLuint &bfUsedTextures)
+{
+    // Save the handles to the 2 framebuffers
+    mFboId[0] = fbos[0];
+    mFboId[1] = fbos[1];
+    mInitTexId = initTexture;
+
+    // Initialize all OpenGL structures necessary for the
+    // labeling phase here
+    mRead  = read;
+    mWrite = write;
+
+    // Load the shaders and get a linked program object
+    mProgramObject = loadProgramFromFile( mVertFilename, mFragFilename);
+    if (mProgramObject == 0)
+    {
+        cerr << "Failed to generate Program object for reduction phase" << endl;
+    }
+
+     // Get the attribute locations
+     mPositionLoc = glGetAttribLocation ( mProgramObject, "a_position" );
+     mTexCoordLoc = glGetAttribLocation ( mProgramObject, "a_texCoord" );
+
+     // Get the sampler locations
+     mSamplerLoc     = glGetUniformLocation( mProgramObject, "s_texture" );
+     u_texDimLoc     = glGetUniformLocation ( mProgramObject, "u_texDimensions" );
+     u_passLoc       = glGetUniformLocation ( mProgramObject, "u_pass" );
+     u_debugLoc      = glGetUniformLocation ( mProgramObject, "u_debug" );
+
+     // Create numNewTextures texture for the frambuffer and
+     // bind the textures to the corresponding fbo
+     if(numNewTextures > 1)
+     {
+         mFboTexId[0]  = createSimpleTexture2D(mWidth, mHeight, mTgaData->img_data);
+         GL_CHECK( glBindFramebuffer(GL_FRAMEBUFFER, mFboId[0]) );
+         int i = 0;
+         while( (1<<i) & bfUsedTextures) ++i;
+
+         GL_CHECK( glActiveTexture( GL_TEXTURE0 + i) );
+         bfUsedTextures |= (1<<i);
+         GL_CHECK( glBindTexture(GL_TEXTURE_2D, mFboTexId[0]) );
+         GL_CHECK( glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, mFboTexId[0], 0) );
+         GLenum status = glCheckFramebufferStatus(GL_FRAMEBUFFER);
+         if (status != GL_FRAMEBUFFER_COMPLETE)
+         {
+             printf("Framebuffer is not complete with %08x\n", status);
+         }
+     }
+
+     if (numNewTextures > 0)
+     {
+         mFboTexId[1]  = createSimpleTexture2D(mWidth, mHeight);
+         int i = 0;
+         while( (1<<i) & bfUsedTextures) ++i;
+
+         GL_CHECK( glActiveTexture( GL_TEXTURE0 + i) );
+         bfUsedTextures |= (1<<i);
+
+         GL_CHECK( glBindTexture(GL_TEXTURE_2D, mFboTexId[1]) );
+         GL_CHECK( glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, mFboTexId[1], 0) );
+         GLenum status = glCheckFramebufferStatus(GL_FRAMEBUFFER);
+         if (status != GL_FRAMEBUFFER_COMPLETE)
+         {
+             printf("Framebuffer is not complete with %08x\n", status);
+         }
+     }
+
+     GL_CHECK( glClearColor ( 0.0f, 0.0f, 0.0f, 0.0f ) );
+
+     return GL_TRUE;
+}
+
+void ReductionPhase::setupGeometry()
+{
+    //
+}
+
+void ReductionPhase::run()
+{
+    //
+}
---
+++ reductionPhase.h
@@ -0,0 +1,63 @@
+#ifndef REDUCTIONPHASE_H
+#define REDUCTIONPHASE_H
+
+#include "phase.h"
+
+class ReductionPhase : public Phase
+{
+public:
+
+    // Vertex and fragment shader files
+    const char * mVertFilename;
+    const char * mFragFilename;
+
+    // Handle to a program object
+    GLuint mProgramObject;
+
+    int mWidth;
+    int mHeight;
+
+    // Attribute locations
+    GLint  mPositionLoc;
+    GLint  mTexCoordLoc;
+
+    // Vertices
+    GLfloat mVertices[20];
+    GLushort mIndices[6];
+
+    // Uniform locations
+    GLint  u_texDimLoc;
+    GLint  u_passLoc;
+    GLint  u_debugLoc;
+
+    // Uniform values
+    GLint u_pass;
+    GLint u_debug;
+
+//  Check is all members necessary
+    // Sampler location
+    GLint mSamplerLoc;
+
+    // Texture handle
+    GLuint mTextureId;
+    /// TODO: tga somewhere else?
+    TGA    *mTgaImage;
+    TGAData *mTgaData;
+
+// End Check
+    // Texture to attach to the frambuffers
+    GLuint mFboTexId[2];
+    GLuint mInitTexId;
+    GLuint mFboId[2];
+
+    int mWrite;
+    int mRead;
+
+    ReductionPhase(int width = 0, int height = 0);
+    GLint init(GLuint fbos[], GLuint initTexture, int numNewTextures, GLuint &bfUsedTextures);
+
+    void setupGeometry();
+    virtual void run();
+};
+
+#endif // REDUCTIONPHASE_H
