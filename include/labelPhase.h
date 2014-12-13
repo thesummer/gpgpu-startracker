09f6887168cbc52c2de6032fa1dd82a48e3ea2af
Jan Sommer <summerspam@web.de>
2014-12-13 12:34:28 +0100

    Add doxygen comments to labelPhase.h

--- include/labelPhase.h
+++ include/labelPhase.h
@@ -7,71 +7,185 @@
 #include "tga.h"
 #include "getTime.h"
 
+/*!
+ \brief Contains all handles and functions for the labeling phase
+
+ This class is responsible for the thresholding of the initial image and the
+ labeling of the remaining spots.
+ A more elaborate explanation of the GLSL-algorithm can be found TODO.
+
+ NOTE: The handles for shader-, texture-, fbo-objects etc. are public at the moment
+ for easy debugging purposes. Probably would be a good idea to move them to
+ private later.
+
+
+
+*/
+/*!
+ \brief
+
+*/
 class LabelPhase: public Phase
 {
 public:
     // Vertex and fragment shader files
-    const char * mVertFilename;
-    const char * mFragFilename;
+    const char * mVertFilename; /*!< Path to the vertex shader file */
+    const char * mFragFilename; /*!< Path to the fragment shader file */
 
     // Handle to a program object
-    GLuint mProgramObject;
+    GLuint mProgramObject; /*!< Handle to the program object */
 
-    int mWidth;
-    int mHeight;
+    int mWidth; /*!< Width of the scene*/
+    int mHeight; /*!< Height of the scene */
 
     // Attribute locations
-    GLint  mPositionLoc;
-    GLint  mTexCoordLoc;
+    GLint  mPositionLoc; /*!< Handle for the attribute a_position*/
+    GLint  mTexCoordLoc; /*!< Handle for the attribute a_texCoord */
 
     // Vertices
-    GLfloat mVertices[20];
-    GLushort mIndices[6];
+    GLfloat mVertices[20]; /*!< Vertex and texture coordinates for the plain quad*/
+    GLushort mIndices[6]; /*!< Indices for the quad scene*/
 
     // Uniform locations
-    GLint  u_texDimLoc;
-    GLint  u_thresholdLoc;
-    GLint  u_passLoc;
-    GLint  u_debugLoc;
-    GLint  u_factorLoc;
+    GLint  u_texDimLoc; /*!< Handle to the uniform u_texDimensions */
+    GLint  u_thresholdLoc; /*!< Handle to the uniform u_threshold */
+    GLint  u_passLoc; /*!< Handle to the uniform u_pass*/
+    GLint  u_debugLoc; /*!< Handle to the uniform u_debug TODO: remove*/
+    GLint  u_factorLoc; /*!< Handle to the uniform u_factor*/
 
     // Uniform values
-    float u_threshold;
-    GLint u_pass;
-    GLint u_debug;
-    GLint u_factor;
+    float u_threshold; /*!< Value of uniform u_threshold */
+    GLint u_pass; /*!< Value of uniform u_pass */
+    GLint u_debug; /*!< Value of uniform u_debug TODO: remove*/
+    GLint u_factor; /*!< Value of uniform u_factor */
 
     // Sampler location
-    GLint mSamplerLoc;
+    GLint mSamplerLoc; /*!< Handle to the sampler s_texture */
 
     // Texture handle
     /// TODO: tga somewhere else?
-    TGA    *mTgaImage;
-    TGAData *mTgaData;
+    TGA    *mTgaImage; /*!< Handle for the loaded tga image */
+    TGAData *mTgaData; /*!< Buffer holding the loaded image data */
 
     // Texture to attach to the frambuffers
-    GLuint mTexOrigId;
-    GLuint mTexPiPoId[2];
-    GLuint mFboId[2];
-    GLint  mTextureUnits[3];
+    GLuint mTexOrigId; /*!< Handle to the texture which holdes the original image*/
+    GLuint mTexPiPoId[2]; /*!< Handle to the two textures which are used for ping-pong-method*/
+    GLuint mFboId[2]; /*!< Handles to the two FBOs for ping-pong method*/
+    GLint  mTextureUnits[3]; /*!< Handles to the texture units for the above textures*/
 
-    int mWrite;
-    int mRead;
+    int mWrite; /*!< Holds the index of the FBO/texture which is written to */
+    int mRead; /*!< Holds the index of the texture which is read from */
 
+    /*!
+     \brief Constructor
+
+     Initializes the name of the vertex and fragment shader, vertex and
+     texture coordinates of the vertices, the indices of the quad and
+     the value for the threshold operation.
+
+     \param width  Width of the scene
+     \param height Height of the scene
+    */
     LabelPhase(int width = 0, int height = 0);
+
+    /*!
+     \brief Destructor
+
+    */
     virtual ~LabelPhase();
+
+    /*!
+     \brief Initializes the scene and all necessary objects (except for the texture holding the original image)
+
+     Assumes that the texture and texture unit which holds the original image
+     is set from the parent, before \ref run is called.
+
+     \param fbos[] The 2 framebuffers necessary for rendering
+     \param bfUsedTextures The bitfield to determine which texture units are already used
+     \return GLint Returns GL_TRUE on success
+    */
     GLint init(GLuint fbos[], GLuint &bfUsedTextures);
+
+    /*!
+     \brief Initializes the scene and all necessary objects
+
+     This init-function is used for if the phase should be run without the need
+     of the results of any previous stages or input. The image data of the
+     original image is copied from \ref mTgaData->`
+
+     \param fbos[] The 2 framebuffers necessary for rendering
+     \param bfUsedTextures The bitfield to determine which texture units are already used
+     \return GLint Returns GL_TRUE on success
+    */
     GLint initIndependent(GLuint fbos[], GLuint &bfUsedTextures);
 
+
+    /*!
+     \brief Return the handle to the texture holding the original image
+
+     \return GLuint
+    */
     GLuint getOrigTexture();
+
+    /*!
+     \brief Return the index of the texture unit the original texture is assinged to
+
+     \return GLint
+    */
     GLint getOrigTexUnit();
+
+
+    /*!
+     \brief Return the handle to the texture holding the result of the phase
+
+     The last texture is the texture which holds the result of the last iteration
+     of the labeling algorithm, i.e. it holds all identified spots with their
+     assigned labels.
+
+     \return GLuint
+    */
     GLuint getLastTexture();
+
+    /*!
+     \brief Return the index of the texture unit the result texture is assinged to
+
+     \return GLint
+    */
     GLint getLastTexUnit();
+
+    /*!
+     \brief Return the handle to the texture which holds now important data and can be reused
+
+     The free texture holds data from the iteration before the last one which
+     served as input of the last labeling iteration. It is old data, therefore
+     the texture and texture unit can be reused by subsequent phases.
+
+     \return GLuint
+    */
     GLuint getFreeTexture();
+
+    /*!
+     \brief Return the index of the texture unit the free texture is assinged to
+
+     \return GLint
+    */
     GLint getFreeTexUnit();
 
+    /*!
+     \brief Sets up the Viewport and the quad scene
+
+    */
     void setupGeometry();
 
+    /*!
+     \brief Runs the labeling algorithm
+
+     TODO: Cleanup the code and comments
+     TODO: Write short explanation here?
+     TODO: refere to the general explanation and GLSL docu
+
+     \return double The time (in ms) the computation took
+    */
     virtual double run();
 };
 
