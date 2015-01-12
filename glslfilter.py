fda4938cc13e51b80b92b4cb445c3f9fd43102ad
Jan Sommer <summerspam@web.de>
2015-01-12 22:23:05 +0100

    Customize doxygen filter for glsl-files

--- glslfilter.py
+++ glslfilter.py
@@ -62,105 +62,115 @@ re_blockcode_start = re.compile('(?<=[\*]class\s)\w+', re.I | re.VERBOSE)
 ##run regex on a single line
 # @returns either a found result or None
 def getRegSearchLine(str, regex):
-	search = regex.search(str)
-	if search is not None:
-		return search.group(0)
-	return None
+    search = regex.search(str)
+    if search is not None:
+        return search.group(0)
+    return None
 
 ##run regex on an string array
 # @returns either a found result or None
 def getRegSearch(txt, regex):
-	for str in txt:
-		search = regex.search(str)
-		if search is not None:
-			return search.group(0)
-	return None
+    for str in txt:
+        search = regex.search(str)
+        if search is not None:
+            return search.group(0)
+    return None
 
 # generate a class name from filename
 # @return just the filename - no extension and no path
 def generateName(filename):
-	root, ext = os.path.splitext(filename)
-	head, tail = os.path.split(root)
-	return tail
-	
+    root, ext = os.path.splitext(filename)
+    head, tail = os.path.split(root)
+    return tail
+    
 def writeLine(txt):
-	sys.stdout.write(txt + "\n")
-	
+    sys.stdout.write(txt + "\n")
+    
 ## parse a shader and generate needed information along on the way
 ## - if comments contain a namespace move it the classname
 def parseShader(filename, txt, type = None):
-	# extract name from doxygen-tag or use generic GLSL namespace
-	namespace = getRegSearch(txt, re_doxy_namespace)
-	if namespace is None:
-		namespace = "GLSL"
-	else:
-		#remove namespace line from txt
-		txt = [str for str in txt if getRegSearchLine(str, re_doxy_namespace) is None]
+    # extract name from doxygen-tag or use generic GLSL namespace
+    namespace = getRegSearch(txt, re_doxy_namespace)
+    if namespace is None:
+        namespace = "GLSL"
+    else:
+        #remove namespace line from txt
+        txt = [str for str in txt if getRegSearchLine(str, re_doxy_namespace) is None]
 
-	# extract className from doxygen-tag or use filename
-	className = getRegSearch(txt, re_doxy_class)
-	if className is None:
-		className = generateName(filename)
-	else:
-		#remove calssName line from txt
-		txt = [str for str in txt if getRegSearchLine(str, re_doxy_class) is None]
-	
-	comment = []
-	if len(txt) > 0:
-		if txt[0].find("/*") >=0:
-			line = txt.pop(0)
-			while (line.find("*/") < 0) and (len(txt) > 0):
-				comment.append(line)
-				line = txt.pop(0)
-			comment.append(line)
-				
-	
+    # extract className from doxygen-tag or use filename
+    className = getRegSearch(txt, re_doxy_class)
+    if className is None:
+        className = generateName(filename)
+    else:
+        #remove calssName line from txt
+        txt = [str for str in txt if getRegSearchLine(str, re_doxy_class) is None]
+    
+    comment = []
+    hasGroup = False
+    if len(txt) > 0:
+        if txt[0].find("/*") >=0:
+            line = txt.pop(0)
+            while (line.find("*/") < 0) and (len(txt) > 0):
+                #if (line.find("ingroup") < 0):
+                    #hasGroup = True
+                comment.append(line)
+                line = txt.pop(0)
+            comment.append(line)
+    #if (hasGroup):
+        #showLines(comment)
+        #if len(txt) > 0:
+            #if txt[0].find("/*") >=0:
+                #line = txt.pop(0)
+                #while (line.find("*/") < 0) and (len(txt) > 0):
+                    #comment.append(line)
+                    #line = txt.pop(0)
+                #comment.append(line)
+    
 
-	# dump the file and pad it with namespace/name class information
-	# 1st: namespace + class padding, also declare everything public
-	writeLine("/** @namespace " + namespace + " */")
-	writeLine("public class " + namespace+"::"+className + "{")
-	writeLine("public:")
-	# 2nd: dump original commentblock
-	showLines(comment)
-	# 3rd: add type-remark and classname
-	if type is not None:
-		writeLine("/** @remark <b>" + type + "</b> */")
-	writeLine("/** @class " + namespace+"::"+className + " */")
-	# 4th: dump remaining file 
-	showLines(txt)
-	# 5th: close dummy class
-	writeLine("}")
+    showLines(comment)
+    # dump the file and pad it with namespace/name class information
+    # 1st: namespace + class padding, also declare everything public
+    writeLine("public class " + className + "{")
+    writeLine("public:")
+    # 2nd: dump original commentblock
+    # 3rd: add type-remark and classname
+    if type is not None:
+        writeLine("/** @remark <b>" + type + "</b> */")
+    writeLine("/** @class " + namespace+"::"+className + " */")
+    # 4th: dump remaining file 
+    showLines(txt)
+    # 5th: close dummy class
+    writeLine("}")
 
 ## @returns the complete file content as an array of lines
 def readFile(filename):
-	f = open(filename)
-	r = f.readlines()
-	f.close()
-	return r
-	
+    f = open(filename)
+    r = f.readlines()
+    f.close()
+    return r
+    
 ## dump all lines to stdout
 def showLines(r):
-	for s in r:
-		sys.stdout.write(s)
-		
+    for s in r:
+        sys.stdout.write(s)
+        
 ## main method - open a file and see what can be done
 def filter(filename):
-	try:
-		root, ext = os.path.splitext(filename)
-		txt = readFile(filename)
-		if (ext.lower() == ".frag" or ext.lower() == ".glsl"):
-			parseShader(filename, txt, "Fragment-Shader")
-		elif (ext.lower() == ".vert"):
-			parseShader(filename, txt, "Vertex-Shader")
-		else:
-			showLines(txt)
-	except IOError,e:
-		sys.stderr.write(e[1]+"\n")
+    try:
+        root, ext = os.path.splitext(filename)
+        txt = readFile(filename)
+        if (ext.lower() == ".frag" or ext.lower() == ".glsl"):
+            parseShader(filename, txt, "Fragment-Shader")
+        elif (ext.lower() == ".vert"):
+            parseShader(filename, txt, "Vertex-Shader")
+        else:
+            showLines(txt)
+    except IOError,e:
+        sys.stderr.write(e[1]+"\n")
 
 if len(sys.argv) != 2:
-	print "usage: ", sys.argv[0], " filename"
-	sys.exit(1)
+    print "usage: ", sys.argv[0], " filename"
+    sys.exit(1)
 
 filename = sys.argv[1] 
 filter(filename)
